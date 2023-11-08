a1fd00c448d298799dd09b22dd76778f51a988cd
Aloke Pramanik <129755834+hackerbotsupreme@users.noreply.github.com>
2023-11-08 08:25:39 +0530

    Add files via upload

---
+++ 2.solution2.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=[]
+for i in range(n):
+    s=int(input())
+    l.append(s)
+for i in l:
+    if i >=80:
+        print("yes")
+    if i<80 :
+        print("no")
+# i have not decoded it yet gotta do it later 
+     
